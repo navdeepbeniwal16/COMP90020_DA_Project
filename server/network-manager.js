3ccdd060af0b747c84aa65775146901e1f6cba48
Abdul Hannan Ghafoor <aghafoor@student.unimelb.edu.au>
2023-05-04 23:42:23 +1000

    merging the changes

--- server/network-manager.js
+++ server/network-manager.js
@@ -19,14 +19,20 @@ if(!PORT) {
 
 workernodes = {};
 let producerNode = null;
-let validatorNodes = [];
+let validatorNodes = {};
 let validationsReceived = 0;
 let isBlockValidated = false;
+let validatorPercentage = 10;
+let stakeReward = 2;
 
+// stake generator
+function stakegenerator(){
+    return Math.floor(Math.random()*100);
+}
 // registering/deregistering a blockchain node
 ioServer.on('connection', (socket) => {
     console.log(`Blockchain node with id : ${socket.id} registering with network-manager`);
-    workernodes[socket.id] = socket;
+    workernodes[socket.id] = {"socket":socket,"stake":stakegenerator()};
     
     socket.on('disconnect', () => {
         console.log(`Blockchain node with id : ${socket.id} deregistering with network-manager`);
@@ -37,8 +43,8 @@ ioServer.on('connection', (socket) => {
         console.log(`Forged block received from the ${socket.id}`);
         console.log(forgedBlock);
 
-        for(let nIndex=0; nIndex < validatorNodes.length; nIndex++) {
-            const node = workernodes[validatorNodes[nIndex]];
+        for(nodeId in validatorNodes) {
+            const node = validatorNodes[nodeId]['socket'];
             node.emit('validate-block', JSON.parse(JSON.stringify(forgedBlock)));
         }
     })
@@ -47,11 +53,11 @@ ioServer.on('connection', (socket) => {
         console.log(`Blockchain node with id : ${socket.id} validated the block`);
         validationsReceived++;
 
-        if(validationsReceived >= (2/3) * validatorNodes.length && !isBlockValidated) {
+        if(validationsReceived >= (2/3) * Object.keys(validatorNodes).length && !isBlockValidated) {
             isBlockValidated = true;
             const nodeIds = Object.keys(workernodes);
             for(let nIndex=0; nIndex < nodeIds.length; nIndex++) {
-                const node = workernodes[nodeIds[nIndex]];
+                const node = workernodes[nodeIds[nIndex]]['socket'];
                 node.emit('commit-block');
             }
         }
@@ -63,21 +69,66 @@ function getTransactionBlockLength() {
     return transactionqueue.length;
 }
 
+function pickProducerNode(validatorsArg) {
+    const validatorNodesIds = Object.keys(validatorsArg);
+    if(workerNodesIds.length === 0) {
+        throw new Error('No worker nodes registered on the system');
+    }
+    MaxStake = 0;
+    var producerSocket = null;
+    for (nodesockets in validatorsArg){
+        if (validatorsArg[nodesockets]["stake"]>MaxStake){
+            MaxStake = validatorsArg[nodesockets]["stake"];
+            producerSocket = nodesockets;
+        }
+    }
+    //const chosenNodeId = workerNodesIds[Math.floor(Math.random() * workerNodesIds.length)];
+    workernodes[ProducerSocket]['stake']=workernodes[ProducerNode]['stake']+stakeReward;
+    return validatorsArg[producerSocket]['socket'];
+}
+
+function pickValidatorsNodes(){
+    const workerNodesCopy = {};
+    Object.assign(workerNodesCopy, workernodes);
+    const workerNodesIds = Object.keys(workerNodesCopy);
+    const sizeOfWorkerNodes = Object.keys(workerNodesCopy).length
+    const sizeOfValidatorsNodes = Math.ceil(sizeOfWorkerNodes*validatorPercentage/100);
+    if(workerNodesIds.length === 0) {
+        throw new Error('No worker nodes registered on the system');
+    }
+    else if (sizeOfValidatorsNodes == 0){
+        sizeOfValidatorsNodes = 1;
+    }
+    
+    console.log("this is the else statement of the validator function");
+        
+    validators = {}
+    for (let i = 0; i < sizeOfValidatorsNodes; i++){
+        const chosenNodeId = workerNodesIds[Math.floor(Math.random() * workerNodesIds.length)]
+        validators[chosenNodeId] = workerNodesCopy[chosenNodeId];
+        workerNodesIds.splice(chosenNodeId,1);
+    }
+    
+    return validators;
+}
+
 function conductElection() {
     const workerNodesIds = Object.keys(workernodes);
     if(workerNodesIds.length === 0) {
         throw new Error('No worker nodes registered on the system');
     }
     
-    const chosenNodeId = workerNodesIds[Math.floor(Math.random() * workerNodesIds.length)];
-    producerNode = workernodes[chosenNodeId]
+    //const chosenNodeId = workerNodesIds[Math.floor(Math.random() * workerNodesIds.length)];
+    validatorNodes = pickValidatorsNodes();
+    producerNode = pickProducerNode(validatorNodes);
 
-    validatorNodes = [];
+    /*
     for(let nIndex=0; nIndex < workerNodesIds.length; nIndex++) {
         if(workerNodesIds[nIndex] !== chosenNodeId) {
             validatorNodes.push(workerNodesIds[nIndex]);
         }
     }
+    */
 }
 
 // Rest APIs to access the network-manager
@@ -112,7 +163,7 @@ app.post('/network/transactions', (req,res) => {
             if (Object.keys(workernodes).length > 0){
                 console.log("Sending transaction block to blockchain nodes to be pooled.");
                 for(const nodeId in workernodes){
-                    const node = workernodes[nodeId];
+                    const node = workernodes[nodeId]['socket'];
                     console.log("Sending to Node id : "  + node.id);
                     node.emit('pool-transaction-block', transactionblock)
                 }  
